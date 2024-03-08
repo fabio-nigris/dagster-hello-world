95f4b632882cf8257e22299bb117ed9f4acca929
fabio-nigris <fabio.nigris@shell.com>
2024-03-08 14:20:17 +0100

    Update hello_world_job function to use hello_world_1 and hello_world_2

--- jobs.py
+++ jobs.py
@@ -1,7 +1,8 @@
 from dagster import job
 
-from ops import hello_world
+from ops import hello_world_1, hello_world_2
 
 @job
 def hello_world_job():
-    hello_world()
+    start = hello_world_1()
+    hello_world_2(start=start)
--- ops.py
+++ ops.py
@@ -1,5 +1,10 @@
-from dagster import op
+from dagster import op, In, Nothing, OpExecutionContext
 
 @op
-def hello_world():
-    return 'Hello, World!'
+def hello_world_1(context: OpExecutionContext):
+    context.log.info('Hello, World 1!')
+
+@op(ins={"start": In(Nothing)})
+def hello_world_2(context: OpExecutionContext):
+    context.log.info('Hello, World 2!')
+
